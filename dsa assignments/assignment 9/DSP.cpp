7b3cf066992233c9320be6a3a7bf4dd7ed80dae6
Hardik Pajiala <hardikpajiala@gmail.com>
2025-11-26 00:12:25 +0530

    Update DSP.cpp

--- dsa assignments/assignment 9/DSP.cpp
+++ dsa assignments/assignment 9/DSP.cpp
@@ -1,42 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const long long INF = (1LL<<60);
+long long dist[100005];
+int vis[100005];
+pair<int, long long> adj[100005][100];
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, m;
     if (!(cin >> n >> m)) return 0;
-    const long long INF = (1LL<<60);
-    vector<pair<int,long long>> adj[100005];
+
+    int edges[100005] = {0};
     for (int i = 0; i < m; ++i) {
         int u, v;
         long long w;
         cin >> u >> v >> w;
-        adj[u].push_back({v, w});
-        adj[v].push_back({u, w});
+        adj[u][edges[u]++] = {v, w};
+        adj[v][edges[v]++] = {u, w};
     }
+
     int src;
     cin >> src;
-    vector<long long> dist(n+1, INF);
-    vector<int> vis(n+1, 0);
-    priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
+    for (int i = 1; i <= n; ++i) dist[i] = INF;
+
+    priority_queue<pair<long long, int>, 
+        vector<pair<long long, int>>, 
+        greater<pair<long long, int>>> pq;
+
     dist[src] = 0;
     pq.push({0, src});
+
     while (!pq.empty()) {
-        auto [d,u] = pq.top(); pq.pop();
+        int u = pq.top().second;
+        long long d = pq.top().first;
+        pq.pop();
+
         if (vis[u]) continue;
         vis[u] = 1;
-        for (auto &e : adj[u]) {
-            int v = e.first; long long w = e.second;
+
+        for (int i = 0; i < edges[u]; i++) {
+            int v = adj[u][i].first;
+            long long w = adj[u][i].second;
+
             if (!vis[v] && d + w < dist[v]) {
                 dist[v] = d + w;
                 pq.push({dist[v], v});
             }
         }
     }
+
     for (int i = 1; i <= n; ++i) {
         if (i > 1) cout << " ";
-        if (dist[i] == INF) cout << "INF"; else cout << dist[i];
+        if (dist[i] == INF) cout << "INF";
+        else cout << dist[i];
     }
     cout << '\n';
     return 0;
