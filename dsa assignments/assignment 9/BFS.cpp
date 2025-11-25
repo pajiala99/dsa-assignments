ee37121b6b50dfa0065f13a0be3d4ddcc87be564
Hardik Pajiala <hardikpajiala@gmail.com>
2025-11-26 00:07:17 +0530

    Update BFS.cpp

--- dsa assignments/assignment 9/BFS.cpp
+++ dsa assignments/assignment 9/BFS.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bfs(int n, vector<int> adj[], int start) {
+void bfs(int n, int adj[][1005], int start) {
     int visited[1005] = {0};
     queue<int> q;
     q.push(start);
@@ -9,9 +9,9 @@ void bfs(int n, vector<int> adj[], int start) {
     while (!q.empty()) {
         int u = q.front(); q.pop();
         cout << u << " ";
-        for (int v : adj[u]) {
-            if (!visited[v]) {
-                visited[v] = 1;
+        for (int v = 1; v <= n; v++) { 
+            if (adj[u][v] && !visited[v]) { 
+visited[v] = 1;
                 q.push(v);
             }
         }
@@ -22,13 +22,13 @@ void bfs(int n, vector<int> adj[], int start) {
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> adj[1005];
+    int adj[1005][1005] = {0}; // Adjacency matrix initialized to 0
 
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u); 
+        adj[u][v] = 1;
+        adj[v][u] = 1; // For undirected graph
     }
 
     int start;
