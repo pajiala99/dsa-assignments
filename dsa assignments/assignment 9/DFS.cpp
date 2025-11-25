c33fc99030598109cf7bfee4a981d0cd01c71225
Hardik Pajiala <hardikpajiala@gmail.com>
2025-11-26 00:09:17 +0530

    Update DFS.cpp

--- dsa assignments/assignment 9/DFS.cpp
+++ dsa assignments/assignment 9/DFS.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 int visited[1005];
 
-void dfsUtil(int u, vector<int> adj[]) {
+void dfsUtil(int u, int adj[][1005], int n) {
     visited[u] = 1;
     cout << u << " ";
-    for (int v : adj[u]) {
-        if (!visited[v]) dfsUtil(v, adj);
+    for (int v = 1; v <= n; v++) {
+        if (adj[u][v] && !visited[v]) dfsUtil(v, adj, n);
     }
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> adj[1005];
+    int adj[1005][1005] = {0};
 
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
     }
 
     int start;
     cin >> start;
 
     memset(visited, 0, sizeof(visited));
-    dfsUtil(start, adj);
+    dfsUtil(start, adj, n);
     return 0;
 }
